3415d98e85dd7433c4070e8972e50f0492bd7dd5
hristakiev1 <axsmarine@abv.bg>
2021-02-27 14:09:00 +0200

    adding header component, logo and links to home, shop and contact

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
-import { Switch, Link, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop-page/shop.component";
+import Header from "./components/navbar/navbar.component";
 
 function App() {
   return (
     <div>
       <h1>Hello Lifty</h1>
+      <Header />
 
       <Switch>
         <Route path="/shop" component={ShopPage} />
---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/brand-icon/logo.svg";
+import "./navbar.style.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <NavLink to="/" className="logo-container">
+        <Logo className="logo" />
+      </NavLink>
+      <div className="options">
+        <NavLink to="/shop" className="option">
+          SHOP
+        </NavLink>
+        <NavLink to="/contact" className="option">
+          CONTACT
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
